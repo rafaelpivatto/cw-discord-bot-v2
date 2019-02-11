6067c45eb2a98c9f461982d50d7662b88fd70f40
Rafael Pivatto <rafael.pivatto@gmail.com>
2019-02-10 22:20:32 -0200

    Add helmet

--- src/server/application.js
+++ src/server/application.js
@@ -2,17 +2,19 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const helmet = require('helmet')
+
 global.appRoot = path.resolve(__dirname, '../../')
 
 app.set('port', process.env.PORT || 5000)
 
-app.disable('x-powered-by')
+// Middlewares
+app.use(helmet())
+app.use('/src/', express.static(`${global.appRoot}/dist`))
 
 app.listen(app.get('port'), () => {
   console.log(`Application has started on port ${app.get('port')}`)
 })
-
-app.use('/src/', express.static(`${global.appRoot}/dist`))
 
 // To load index on server
 app.get('/', function (req, res) {
